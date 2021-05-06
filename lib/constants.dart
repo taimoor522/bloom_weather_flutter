7d1f887627afaeb8f033b79bb09f6f66c37fc8a6
Taimoor Ghafar <khantaimoor522@gmail.com>
2021-05-06 17:15:35 +0500

    git commit -m "final commit"

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Api Key
+const kApiKey = "db1dccb25bc5d349bb85f667180cdbee";
+
+// Color Scheme
+
+const kYellowColor = Color(0xFFFAE177);
+const kOrangeColor = Color(0xFFFFBE94);
+const kNavyBlueColor = Color(0xFF18223C);
+const kLightBlueColor = Color(0xFF69E6E4);
+const kDarkBlueColor = Color(0xFF1565c0);
+const kCardColor = Color(0x55FFFFFF);
+const kBlackColor = Colors.black87;
+
+//////////////////   Color Scheme     ////////////////
